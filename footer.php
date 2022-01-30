68db07a9e1c2b6e25c00f0c217dc33982cd4623c
Arturius771 <fodena@tcd.ie>
2022-01-30 21:45:36 +0000

    added php functionality

---
+++ footer.php
@@ -0,0 +1,2 @@
+<!--Footer-->
+<footer>Copyright &copy; 2021 Artúr Foden. All Rights Reserved.</footer>
---
+++ resume.php
@@ -0,0 +1,33 @@
+<?php include('head.php');?>
+<body>
+<?php include('navbar.php');?>
+<!--Main Content-->
+<div class="main_content">
+    <h1>My CV.</h1>
+    <p>You can download a relatively up to date version of my CV <a href="files/Artúr Foden CV - CS.docx">by clicking here</a>. There you will see a complete version rather than the preview below, with further information on my work experience and <a href="projects.php">projects</a>.</p>
+    <h2>Skills.</h2>
+    <p>Computer Science - I have experience with Java, C#, ABAP, Python, SQL, PHP, HTML/CSS/XML and Javascript, in Windows/Linux environments. I have worked on development projects at work and in college that created solutions in the fields of web applications, website design, databases, interaction and experience design, and software engineering.</p>
+    <p>Communication - My written and spoken English is clear and concise. I have practical experience dealing with customers on the phone, email, and in person and taking the time to be empathetic to their needs and to communicate solutions is a skill I have developed.</p>
+    <p>Organisation - Balancing work and university projects at the same time is important to me, and this involves managing time and the workflow. I set and keep goals for my personal projects. I have work experience in teams and coordinating productivity and output with other people.</p>
+    <p>Innovation - At work I was involved with our location's innovation project to drive creative thinking and adaptation to increase productivity in the workplace. In this role I gained a qualification in Design Thinking and managed events. I also faced the challenges of dealing with corporate inertia to help deal with the deeper issues facing innovation. </p>
+    <h2>Languages.</h2>
+    <p>Java, C#, Python, Javascript, CSS, PHP, SQL, ABAP</p>
+    <h2>University Modules</h2>
+    <ul>
+    <li>TM111 Introduction to Computing and Information Technology 1</li>
+    <li>TM112 Introduction to Computing and Information Technology 2</li>
+    <li>TM129 Technology in Practise</li>
+    <li>MU123 Discovering Mathematics</li>
+    <li>M250 Object Oriented Java Programming</li>
+    <li>M269 Algorithms, Data Structures and Computability</li>
+    <li>TM255 Communication and Information Technologies</li>
+    <li>TT284 Web Technology</li>
+    <li>TM354 Software Engineering</li>
+    <li>TM352 Web, Mobile and Cloud Technologies</li>
+    <li>TM358 Machine Learning and Artificial Intelligence</li>
+    <li>TM470 Computing and IT Project</li>
+    </ul>
+</div>		
+<?php include('footer.php');?>
+</body>
+</html>
