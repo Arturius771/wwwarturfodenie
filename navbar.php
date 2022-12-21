d79cbd6a5bc65ef6e56c7f57047ec58b39f33532
ArturFoden <fodena@tcd.ie>
2022-12-21 04:20:21 +0000

    removed unwanted php

--- navbar.php
+++
@@ -1,9 +0,0 @@
-<?php $activePage = basename($_SERVER['PHP_SELF'], ".php");?>
-<!--Navigation Bar-->
-<div class="navbar">
-    <a href="index.php"><img class="navbar" src="files/logo.png" alt="Clickable image that links to homepage"></a>
-	<a class="navbar <?=($activePage == 'index') ? 'active_link':'';?>" href="index.php">Home</a>
-	<a class="navbar <?=($activePage == 'projects') ? 'active_link':'';?>" href="projects.php">Project Experience</a>
-	<a class="navbar <?=($activePage == 'resume') ? 'active_link':'';?>" href="resume.php">Résumé/Skills</a>
-	<a class="navbar <?=($activePage == 'files') ? 'active_link':'';?>" href="files.php">Files</a>
-</div>
--- php.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*This will simply display the current
-PHP version active on the server
-*/
-phpinfo();
-?>
