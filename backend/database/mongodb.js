a476f41187080d84c0f47d2f1cc7f8cb9630d6b7
ArturFoden <fodena@tcd.ie>
2023-01-04 14:48:33 +0000

    changed to cert

--- backend/database/mongodb.js
+++ backend/database/mongodb.js
@@ -1,14 +1,30 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://ArturFoden1:password@cluster0.xydbj.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log(client);
-  // perform actions on the collection object
-  client.close();
-});
+const fs = require("fs");
+
+const credentials = "./X509-cert-3730141064141644294.pem";
+
+const client = new MongoClient(
+  "mongodb+srv://cluster0.xydbj.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
+  {
+    sslKey: credentials,
+    sslCert: credentials,
+    serverApi: ServerApiVersion.v1,
+  }
+);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
+    const docCount = await collection.countDocuments({});
+    console.log(client);
+    console.log(docCount);
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
