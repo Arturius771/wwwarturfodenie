7bf5fdc3fd19d126a645fa74f640b4a07a419b41
ArturFoden <fodena@tcd.ie>
2023-01-13 13:30:53 +0000

    working backend API

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const User = require("../database/models/user.js");
-const afdbtoken = require("./afdbtoken.js");
+const afdbtoken = require("../database/afdbtoken.js");
 
-token = `mongodb+srv://${afdbtoken.username}:${encodeURIComponent(
-  afdbtoken.password
-)}@cluster0.xydbj.mongodb.net/?retryWrites=true&w=majority`;
+// Schemas
+const User = require("../database/models/user.js");
+
+token = `mongodb+srv://${afdbtoken.username}:${afdbtoken.password}@cluster0.xydbj.mongodb.net/${afdbtoken.database}?retryWrites=true&w=majority`;
 
 console.log(token);
 
@@ -14,7 +14,7 @@ mongoose.set("strictQuery", false);
 
 mongoose.connect(token, (err) => {
   if (err) {
-    console.error("Erro!" + err);
+    console.error("Error!" + err);
   } else {
     console.log("Connected to mongodb");
   }
@@ -25,8 +25,8 @@ router.get("/", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  let userData = req.body;
-  let user = new User(userData);
+  const userData = req.body;
+  const user = new User(userData);
   user.save((error, registeredUser) => {
     if (error) {
       console.log(error);
