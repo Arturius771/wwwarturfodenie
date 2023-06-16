67011323363307e3fa014e7c8b5a66b95a963b6c
ArturFoden <fodena@tcd.ie>
2023-06-17 00:03:13 +0100

    refactor lib

--- src/lib/components/af-image/af-image.component.ts
+++ src/lib/components/af-image/af-image.component.ts
@@ -6,62 +6,6 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
   templateUrl: './af-image.component.html',
   styleUrls: ['./af-image.component.less'],
 })
-export class AfImageComponent implements OnInit {
+export class AfImageComponent {
   @Input() imageSrc: any;
-  @ViewChild('intersect')
-  intersect!: ElementRef;
-  @ViewChild('progress')
-  progress!: ElementRef;
-  @ViewChild('imageEl')
-  imageEl!: ElementRef;
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {}
-
-  observerCallback(
-    entries: string | any[],
-    observer: { unobserve: (arg0: any) => void }
-  ) {
-    for (let i = 0; i < entries.length; i++) {
-      let entry = entries[i];
-      if (entry.isIntersecting && entry.target.className === 'intersect') {
-        entry.target.querySelector('.lds-ring').className = 'hidden';
-        let imgSrc = this.imageEl.nativeElement.getAttribute('data-imagesrc');
-        this.http
-          .get(imgSrc, {
-            responseType: 'blob',
-            reportProgress: true,
-            observe: 'events',
-            headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-          })
-          .subscribe((event) => {
-            if (event.type === HttpEventType.Response) {
-              if (event.status !== 404) {
-                entry.target.className = 'hidden';
-                this.imageEl.nativeElement.src = window.URL.createObjectURL(
-                  event.body!
-                );
-                this.imageEl.nativeElement.className = '';
-              }
-            }
-          });
-        observer.unobserve(entry.target);
-      }
-    }
-  }
-
-  ngAfterViewInit() {
-    let options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 1,
-    };
-
-    let observer = new IntersectionObserver(
-      this.observerCallback.bind(this),
-      options
-    );
-    observer.observe(this.intersect.nativeElement);
-  }
 }
